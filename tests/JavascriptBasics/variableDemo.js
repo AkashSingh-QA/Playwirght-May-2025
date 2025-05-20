deb4dbf5240dc93ea3cb60471ee3f9bdbd2dc213
Akash Singh <akashsarkar71@gmail.com>
2025-05-20 21:58:13 +0530

    first commit

---
+++ tests/JavascriptBasics/variableDemo.js
@@ -0,0 +1,19 @@
+
+/*
+
+*/
+function varDemo()
+{
+    var m = 200;
+    console.log(m)
+    var str = "I am new to Javascript";
+    var bool = true;
+
+    if(true)
+    {
+        var m = 400;
+        console.log(m)
+    }
+    console.log(m);
+}
+varDemo();
